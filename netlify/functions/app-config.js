494d47fe1463d136b369c708f0852a6b6e25627e
mickey621 <90021142+mickey621@users.noreply.github.com>
2026-04-24 10:21:14 +0800

    Update app-config.js

--- netlify/functions/app-config.js
+++ netlify/functions/app-config.js
@@ -1,7 +1,7 @@
 exports.handler = async () => {
   const config = {
-    SUPABASE_URL: process.env.SUPABASE_URL || "",
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY || ""
+    SUPABASE_URL: process.env.SUPABASE_URL || "https://qxgxszsjlkebolrndeas.supabase.co",
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY || "sb_publishable_4mQ2T7yNYBuzpvAq-YhbXA_thRyDhEt"
   };
 
   return {
